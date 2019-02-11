51cd63ab323c92670e467cba477841b81f58e226
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-10 22:42:31 -0500

    Add Package + Enable DevMode for the game scene

---
+++ Concordia-2-Game/Assets/Scripts/Game/Dev Mode/DevModeCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace con2.game
+{
+    public class DevModeCommands : MonoBehaviour
+    {
+        void Start()
+        {
+            var repo = ConsoleCommandsRepository.Instance;
+            repo.RegisterCommand("help", Help);
+
+            repo.RegisterCommand("reload", Reload);
+        }
+
+        public string Help(string[] args)
+        {
+            var help = string.Join(
+                Environment.NewLine,
+                "Such hacking skills, much technology, wow (ง ͠° ͟ل͜ ͡°)ง",
+                "",
+                "List of available commands for the Game scene:",
+                "- 'reload': Reloads the scene");
+            return help;
+        }
+
+        public string Reload(string[] args)
+        {
+            var currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadSceneAsync(currentSceneName);
+            return "Reloaded the scene";
+        }
+    }
+}
