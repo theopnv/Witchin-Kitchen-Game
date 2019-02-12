4a21ffaaec80d1a0c00fe26752380c5dc4af8819
Mark Beiline <markbeiline@gmail.com>
2019-02-12 16:32:10 -0500

    Player now faces in movement input direction (or in body velocity direction if no input) The rotation is still smooth

--- Concordia-2-Game/Assets/Scripts/Fight/FightControls.cs
+++ Concordia-2-Game/Assets/Scripts/Fight/FightControls.cs
@@ -22,14 +22,11 @@ public class FightControls : MonoBehaviour
     [Range(0, 3)]
     public int PlayerIndex;
 
-    public bool UseMouse = true;
-
     private GameObject target;
     private FighterPunch punch;
     private bool canPunch = true;
 
     private Vector3 movementDirection = new Vector3();
-    private Vector3 facingDirection = new Vector3();
 
     private static string INPUT_HORIZONTAL = "Horizontal";
     private static string INPUT_VERTICAL   = "Vertical";
@@ -42,10 +39,6 @@ public class FightControls : MonoBehaviour
     {
         target = gameObject;
         punch = target.GetComponentInChildren<FighterPunch>();
-
-        // Snap to initial object rotation
-        facingDirection.x = target.transform.forward.x;
-        facingDirection.z = target.transform.forward.z;
     }
 
     // Update is called once per frame
@@ -61,25 +54,6 @@ public class FightControls : MonoBehaviour
             movementDirection.Normalize();
         }
         movementDirection *= MovementSpeed;
-
-        // Rotation
-        if (UseMouse)
-        {
-            var playerOnScreen = Camera.main.WorldToScreenPoint(target.transform.position);
-            var mouseOnScreen = Input.mousePosition;
-
-            facingDirection = mouseOnScreen - playerOnScreen;
-            facingDirection.Normalize();
-            facingDirection.z = facingDirection.y;
-            facingDirection.y = 0.0f;
-        }
-        else
-        {
-            facingDirection.x = Input.GetAxisRaw(getInputStringForPlayer(INPUT_HORIZONTAL_RIGHT, PlayerIndex));
-            facingDirection.y = 0.0f;
-            facingDirection.z = Input.GetAxisRaw(getInputStringForPlayer(INPUT_VERTICAL_RIGHT, PlayerIndex));
-            facingDirection.Normalize();
-        }
 
         // Punch
         if (canPunch && Input.GetButtonDown(getInputStringForPlayer(INPUT_PUNCH, PlayerIndex)))
@@ -121,7 +95,10 @@ public class FightControls : MonoBehaviour
         body.WakeUp();
 
         // Friction in XZ only
-        body.velocity -= MovementFriction * Time.deltaTime * bodyVelocityXZ;
+        if (!Mathf.Approximately(0.0f, bodyVelocityXZ.magnitude))
+        {
+            body.velocity -= MovementFriction * Time.deltaTime * bodyVelocityXZ;
+        }
 
         // If player asked for input
         if (!Mathf.Approximately(movementDirection.magnitude, 0.0f))
@@ -142,11 +119,21 @@ public class FightControls : MonoBehaviour
         // Gravity
         body.AddForce(Vector3.down * Gravity, ForceMode.Acceleration);
 
-        // Smooth rotation
-        if (!Mathf.Approximately(0.0f, facingDirection.magnitude))
+        // Smooth rotation according to velocity
+        bool nonZeroVelocity = bodyVelocityXZ.magnitude > 0.1f;
+        bool nonZeroInput = movementDirection.magnitude > 0.1f;
+        if (nonZeroVelocity || nonZeroInput)
         {
+            Vector3 targetRotation = bodyVelocityXZ.normalized;
+
+            // Override by player's desired rotation, if any
+            if (nonZeroInput)
+            {
+                targetRotation = movementDirection.normalized;
+            }
+
             Quaternion facing = new Quaternion();
-            facing.SetLookRotation(facingDirection);
+            facing.SetLookRotation(targetRotation);
             facing = Quaternion.Slerp(target.transform.rotation, facing, RotationSpeed);
 
             target.transform.rotation = facing;
