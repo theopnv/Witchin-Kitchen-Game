799f7f72302a74de2bdb60ed805cde5963ed069d
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-14 11:32:40 -0500

    Prepare Socket IO script

---
+++ Concordia-2-Game/Assets/Scripts/Lobby/AudienceInteractionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class AudienceInteractionManager : MonoBehaviour
+{
+    private SocketIOComponent _Socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Socket = GetComponent<SocketIOComponent>();
+
+        // Small delay between object instantiation and first use.
+        StartCoroutine("Authenticate");
+    }
+
+    private IEnumerator Authenticate()
+    {
+        yield return new WaitForSeconds(1);
+        _Socket.Emit("registerAsHost");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
