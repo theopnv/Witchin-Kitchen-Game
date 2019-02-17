f67ac4ce947970404088f803336a22e35b201192
Nicole Parmentier <nclparmentier@gmail.com>
2019-02-17 10:55:35 -0500

    Decoupled the creation of PlayerIndex from the fight controls, this will be fully handled by the SpawnPlayersController and is step 1 to setting up priority-driven input management (my next thing today!). At that point, FightControls will no longer even need to know its player index. Also added a Tags class so they're centralized and easily accessible.

--- Concordia-2-Game/Assets/Scripts/Game/Spawn/SpawnPlayersController.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Spawn/SpawnPlayersController.cs
@@ -13,18 +13,28 @@ namespace con2.game
 
         [SerializeField] private GameObject _playerPrefab;
 
+        private GameObject[] m_players = new GameObject[PlayersInfo.PlayerNumber];
+
         #endregion
 
         void Start()
         {
+            GameObject managers = GameObject.FindGameObjectWithTag(Tags.MANAGERS_TAG);
+            GlobalFightState gfs = managers.GetComponentInChildren<GlobalFightState>();
+
             for (var i = 0; i < PlayersInfo.PlayerNumber; i++)
             {
                 var player = Instantiate(_playerPrefab, _playerSpawnPositions[i]);
                 player.GetComponent<Renderer>().material.color = PlayersInfo.Color[i];
-                player.GetComponent<FightControls>().PlayerIndex = i;
+                player.GetComponent<FightControls>().SetPlayerIndex(i);
+                m_players[i] = player;
+                gfs.AddFighter(player);
             }
         }
-        
-    }
 
+        public GameObject[] GetPlayers()
+        {
+            return m_players;
+        }
+    }
 }
