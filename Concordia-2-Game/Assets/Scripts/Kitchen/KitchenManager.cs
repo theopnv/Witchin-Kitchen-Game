f87be36162a1744bcda839eae9b875516fa40e0f
Nicole Parmentier <nclparmentier@gmail.com>
2019-02-18 12:35:45 -0500

    WIP kitchen prefabs + manager

---
+++ Concordia-2-Game/Assets/Scripts/Kitchen/KitchenManager.cs
@@ -0,0 +1,28 @@
+﻿using con2.game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenManager : MonoBehaviour
+{
+    public int m_ownerId;
+
+    void Start()
+    {
+        GameObject managers = GameObject.FindGameObjectWithTag(Tags.MANAGERS_TAG);
+        SpawnPlayersController playerSpawner = managers.GetComponentInChildren<SpawnPlayersController>();
+        GameObject owner = playerSpawner.GetPlayerByID(m_ownerId);
+        if (owner == null)
+        {
+            Debug.LogError("Kitchen owner player " + m_ownerId + "not found!");
+        }
+        else
+        {
+            KitchenStation[] stations = GetComponentsInChildren<KitchenStation>();
+            foreach (KitchenStation station in stations)
+            {
+                station.SetOwner(owner);
+            }
+        }
+    }
+}
