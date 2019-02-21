a0565818d94b3476b01932db75f9e6a74c6b0cbc
Nicole Parmentier <nclparmentier@gmail.com>
2019-02-21 08:55:54 -0500

    WIP on AudienceEvents

--- Concordia-2-Game/Assets/Scripts/Game/Spawn/SpawnPlayersController.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Spawn/SpawnPlayersController.cs
@@ -47,6 +47,11 @@ namespace con2.game
             //Inform Camera of which players to track
             GameObject camera = GameObject.FindGameObjectWithTag(Tags.MAIN_CAMERA);
             camera.GetComponent<SharedCamera>().SetPlayers(m_players);
+
+            //Initialize audience events (requires much player info)
+            MainGameManager mgm = managers.GetComponentInChildren<MainGameManager>();
+            AudienceEventManager eventManager = mgm.GetComponent<AudienceEventManager>();
+            eventManager.InitializeValues();
         }
 
         public GameObject[] GetPlayers()
