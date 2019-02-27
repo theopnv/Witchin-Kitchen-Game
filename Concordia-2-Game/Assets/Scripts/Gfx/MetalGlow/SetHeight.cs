00a51e85ee89c8340d2501aff59fe0c41b06ec4a
Ohmnivore <markbeiline@gmail.com>
2019-02-27 14:37:11 -0500

    Added metal glow shader for cauldron
    To use:
    * Add MetalGlow shader to mesh material
    * Setup the 4 glow textures on shader
    * Add the SetHeight script to the object containing the mesh
    * + You can play with the _GlowIntensity slider (no public API yet)

---
+++ Concordia-2-Game/Assets/Scripts/Gfx/MetalGlow/SetHeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetHeight : MonoBehaviour
+{
+    float GlowMeshHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var renderer = GetComponent<MeshRenderer>();
+        GlowMeshHeight = renderer.bounds.extents.y * 2.0f;
+        renderer.sharedMaterial.SetFloat("_GlowHeight", GlowMeshHeight);
+    }
+}
