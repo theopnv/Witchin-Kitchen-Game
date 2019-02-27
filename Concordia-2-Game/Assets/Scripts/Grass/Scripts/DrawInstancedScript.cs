264722d287bc190f3f956899cd445d695f1d9249
Mark Beiline <markbeiline@gmail.com>
2019-02-26 22:57:17 -0500

    Used https://github.com/JavDevGames/MinimalInstancing as base

---
+++ Concordia-2-Game/Assets/Scripts/Grass/Scripts/DrawInstancedScript.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+
+public class DrawInstancedScript : MonoBehaviour
+{
+    const float BATCH_MAX_FLOAT = 1023f;
+    const int BATCH_MAX = 1023;
+
+    public GameObject prefab;
+    public Material meshMaterial;
+    
+    public int width;
+    public int depth;
+
+    public float spacing;
+    public float threshold;
+
+    private MeshFilter mMeshFilter;
+    private MeshRenderer mMeshRenderer;
+    private Vector4[] colorArray;
+    private Vector4[] instancePositionArray;
+    private Matrix4x4[] matrices;
+    private MaterialPropertyBlock propertyBlock;
+
+    private float CurTime = 0.0f;
+    public float WindSpeed = 1.0f;
+    public Vector2 WindDirection = new Vector2();
+    private Texture2D RollingWindTex;
+    private Vector2 RollingWindOffset = new Vector2();
+    private float MeshHeight = 0.0f;
+    
+    void Start()
+    {
+        mMeshFilter = prefab.GetComponent<MeshFilter>();
+        mMeshRenderer = prefab.GetComponent<MeshRenderer>();
+
+        RollingWindTex = GetComponent<PerlinTexture>().getTex();
+
+        InitData();
+    }
+
+    private void InitData()
+    {
+        MeshHeight = mMeshFilter.sharedMesh.bounds.size.y;
+
+        Vector3 pos = new Vector3();
+        Vector3 scale = new Vector3(1, 1, 1);
+
+        int count = width * depth;
+        matrices = new Matrix4x4[count];
+        colorArray = new Vector4[count];
+        instancePositionArray = new Vector4[count];
+        propertyBlock = new MaterialPropertyBlock();
+        
+        Color[] colors = {
+                            HexToColor(0x03A9F4),
+                            HexToColor(0xE1F5FE),
+                            HexToColor(0xB3E5FC),
+                            HexToColor(0x81D4FA),
+                            HexToColor(0x4FC3F7),
+                            HexToColor(0x29B6F6),
+                            HexToColor(0x03A9F4),
+                            HexToColor(0x039BE5),
+                            HexToColor(0x0288D1),
+                            HexToColor(0x0277BD),
+                            HexToColor(0x01579B),
+                            HexToColor(0x80D8FF),
+                            HexToColor(0x40C4FF),
+                            HexToColor(0x00B0FF),
+                            HexToColor(0x0091EA)
+                        };
+
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < depth; ++j)
+            {
+                int idx = i * depth + j;
+
+                matrices[idx] = Matrix4x4.identity;
+
+                pos.x = i * spacing;
+                pos.y = 0.0f;
+                pos.z = j * spacing;
+
+                float curNoise = Mathf.PerlinNoise(pos.x / (float)width, pos.z / (float)depth);
+
+                if (curNoise >= threshold)
+                {
+                    matrices[idx].SetTRS(pos, Quaternion.identity, scale);
+                }
+
+                colorArray[idx] = colors[idx % colors.Length];
+                instancePositionArray[idx] = pos;
+            }
+        }
+    }
+
+    private Color HexToColor(uint hex)
+    {
+        float r = (hex >> 16) & 0xff;
+        float g = (hex >> 8) & 0xff;
+        float b = (hex) & 0xff;
+        Color newColor = new Color(r / 255f, g / 255f, b / 255f);
+
+        return newColor;
+    }
+
+    void Update()
+    {
+        CurTime += Time.deltaTime;
+        RollingWindOffset -= WindDirection * WindSpeed * Time.deltaTime;
+
+        int total = width * depth;
+        int batches = (int)Mathf.Ceil(total / BATCH_MAX_FLOAT);
+
+        for (int i = 0; i < batches; ++i)
+        {
+            int batchCount = Mathf.Min(BATCH_MAX, total - (BATCH_MAX * i));
+
+            int start = Mathf.Max(0, (i - 1) * BATCH_MAX);
+
+            Matrix4x4[] batchedMatrices = GetBatchedMatrices(start, batchCount);
+            Vector4[] batchedColorArray = GetBatchedArray(start, batchCount);
+            Vector4[] batchedInstancePositionArray = GetBatchedInstancePositionArray(start, batchCount);
+
+            propertyBlock.SetVectorArray("_Color", batchedColorArray);
+            propertyBlock.SetVectorArray("_InstancePosition", batchedInstancePositionArray);
+
+            propertyBlock.SetFloat("_CurTime", CurTime);
+            propertyBlock.SetVector("_WindDirection", WindDirection);
+            propertyBlock.SetTexture("_RollingWindTex", RollingWindTex);
+            propertyBlock.SetVector("_RollingWindOffset", RollingWindOffset);
+            propertyBlock.SetFloat("_MeshHeight", MeshHeight);
+
+            Graphics.DrawMeshInstanced(mMeshFilter.sharedMesh, 0, meshMaterial, batchedMatrices, batchCount, propertyBlock);
+        }
+    }
+
+    private Vector4[] GetBatchedArray(int offset, int batchCount)
+    {
+        Vector4[] batchedArray = new Vector4[batchCount];
+
+        for (int i = 0; i < batchCount; ++i)
+        {
+            batchedArray[i] = colorArray[i + offset];
+        }
+
+        return batchedArray;
+    }
+
+    private Vector4[] GetBatchedInstancePositionArray(int offset, int batchCount)
+    {
+        Vector4[] batchedArray = new Vector4[batchCount];
+
+        for (int i = 0; i < batchCount; ++i)
+        {
+            batchedArray[i] = instancePositionArray[i + offset];
+        }
+
+        return batchedArray;
+    }
+
+    private Matrix4x4[] GetBatchedMatrices(int offset, int batchCount)
+    {
+        Matrix4x4[] batchedMatrices = new Matrix4x4[batchCount];
+
+        for (int i = 0; i < batchCount; ++i)
+        {
+            batchedMatrices[i] = matrices[i + offset];
+        }
+
+        return batchedMatrices;
+    }
+}
---
+++ Concordia-2-Game/Assets/Scripts/Grass/Shaders/InstanceColorShader.shader
@@ -0,0 +1,93 @@
+﻿Shader "Custom/MinimalInstancedShader" 
+{
+    Properties
+    {
+        _Color("Color", Color) = (1, 1, 1, 1)
+        _MainTex("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness("Smoothness", Range(0,1)) = 0.5
+        _Metallic("Metallic", Range(0,1)) = 0.0
+
+        _CurTime("Current simulation time", Float) = 0.0
+        _WindDirection("Wind direction", Vector) = (1.0, 0.0, 0.0, 0.0)
+        _RollingWindTex("Rolling wind texture", 2D) = "black" {}
+        _RollingWindOffset("Rolling wind texture", Vector) = (0.0, 0.0, 0.0, 0.0)
+        _MeshHeight("Height of the mesh", Float) = 1.0
+
+        _InstancePosition("Instance position", Vector) = (1.0, 0.0, 0.0, 0.0)
+    }
+
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" }
+        LOD 200
+
+        CGPROGRAM
+
+        #pragma target 3.0
+        #pragma vertex vert
+        #pragma surface surf Standard fullforwardshadows
+        #pragma multi_compile_instancing
+
+        #include "UnityCG.cginc"
+
+        sampler2D _MainTex;
+        half _Glossiness;
+        half _Metallic;
+
+        float _CurTime;
+        float4 _WindDirection;
+        sampler2D _RollingWindTex;
+        float4 _RollingWindOffset;
+        float _MeshHeight;
+
+		struct Input
+		{
+			float4 vertex : SV_POSITION;
+            float2 uv_MainTex;
+		};
+
+		UNITY_INSTANCING_BUFFER_START(Props)
+		UNITY_DEFINE_INSTANCED_PROP(float4, _Color)
+		UNITY_DEFINE_INSTANCED_PROP(float4, _InstancePosition)
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void vert(inout appdata_full v)
+		{
+			UNITY_SETUP_INSTANCE_ID(v);
+
+            float4 outVertex = v.vertex;
+
+            float4 instancePosition = UNITY_ACCESS_INSTANCED_PROP(Props, _InstancePosition);
+            float offset = dot(instancePosition.xyz, float3(5.0f, 5.0f, 5.0f));
+            float windStrength = sin(_CurTime + offset) + cos(_CurTime + offset);
+            windStrength *= 0.4f;
+
+            //windStrength = 0.0f;
+            float2 rollingWindSampleCoords = instancePosition.xz * 0.001f + _RollingWindOffset;
+            fixed4 rollingWind = tex2Dlod(_RollingWindTex, float4(rollingWindSampleCoords.xy, 0, 0));
+            windStrength += rollingWind.x;
+            windStrength = min(1.0f, windStrength);
+
+            float bendFactor = (v.vertex.y / _MeshHeight);
+            bendFactor *= bendFactor;
+            windStrength *= bendFactor;
+
+            float4 localWindDirection = mul(unity_WorldToObject, _WindDirection);
+            outVertex.xyz += localWindDirection.xyz * windStrength * 2.0f;
+
+			v.vertex = outVertex;
+		}
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            float4 color = UNITY_ACCESS_INSTANCED_PROP(Props, _Color);
+
+            fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * color;
+            o.Albedo = c.rgb;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+		ENDCG
+	}
+}
