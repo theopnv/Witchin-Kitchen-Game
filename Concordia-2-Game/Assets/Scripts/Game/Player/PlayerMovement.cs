5d4f08bf66434e3a0ed96397de263c88fa7d4913
Nicole Parmentier <nclparmentier@gmail.com>
2019-02-27 14:40:01 -0500

    Punching works with physics

--- Concordia-2-Game/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -92,7 +92,7 @@ public class PlayerMovement : MonoBehaviour, IInputConsumer, IPunchable
 
     public void Punch(Vector3 knockVelocity, float stunTime)
     {
-        m_rb.AddForce(-knockVelocity, ForceMode.Impulse);
+        m_rb.AddForce(knockVelocity, ForceMode.VelocityChange);
         //m_rb.velocity = knockVelocity;
         m_stun.Stun(stunTime);
     }
