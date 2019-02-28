508444b68f9e9a8a222b533ad8df90813934be45
Mark Beiline <markbeiline@gmail.com>
2019-02-28 13:13:51 -0500

    Added colormap for grass
    Last thing remaining for MVP: density map

--- Concordia-2-Game/Assets/Scripts/Grass/Scripts/DrawInstancedScript.cs
+++ Concordia-2-Game/Assets/Scripts/Grass/Scripts/DrawInstancedScript.cs
@@ -12,7 +12,8 @@ public class DrawInstancedScript : MonoBehaviour
     public GameObject target;
     public GameObject prefab;
     public Material meshMaterial;
-    
+    public Texture2D colorMap;
+
     public float spacing;
     public float threshold;
 
@@ -74,24 +75,6 @@ public class DrawInstancedScript : MonoBehaviour
         Vector3 scale = new Vector3(1, 1, 1);
         
         propertyBlock = new MaterialPropertyBlock();
-        
-        Color[] colors = {
-                            HexToColor(0x03A9F4),
-                            HexToColor(0xE1F5FE),
-                            HexToColor(0xB3E5FC),
-                            HexToColor(0x81D4FA),
-                            HexToColor(0x4FC3F7),
-                            HexToColor(0x29B6F6),
-                            HexToColor(0x03A9F4),
-                            HexToColor(0x039BE5),
-                            HexToColor(0x0288D1),
-                            HexToColor(0x0277BD),
-                            HexToColor(0x01579B),
-                            HexToColor(0x80D8FF),
-                            HexToColor(0x40C4FF),
-                            HexToColor(0x00B0FF),
-                            HexToColor(0x0091EA)
-                        };
 
         var bounds = target.GetComponent<MeshRenderer>().bounds;
         var w = Mathf.RoundToInt(bounds.size.x - mMeshRenderer.bounds.size.x * 2.0f);
@@ -141,23 +124,15 @@ public class DrawInstancedScript : MonoBehaviour
                 {
                     currentBucket.MatrixArray[currentBucket.NumInstances].SetTRS(pos, Quaternion.identity, scale);
                 }
-                
-                currentBucket.ColorArray[currentBucket.NumInstances] = colors[idx % colors.Length];
+
+                var wProgress = (pos.x - startX) / w;
+                var dProgress = (pos.z - startZ) / d;
+                currentBucket.ColorArray[currentBucket.NumInstances] = colorMap.GetPixelBilinear(wProgress, dProgress);
                 currentBucket.PositionArray[currentBucket.NumInstances] = pos;
 
                 currentBucket.NumInstances++;
             }
         }
-    }
-
-    private Color HexToColor(uint hex)
-    {
-        float r = (hex >> 16) & 0xff;
-        float g = (hex >> 8) & 0xff;
-        float b = (hex) & 0xff;
-        Color newColor = new Color(r / 255f, g / 255f, b / 255f);
-
-        return newColor;
     }
 
     void Update()
