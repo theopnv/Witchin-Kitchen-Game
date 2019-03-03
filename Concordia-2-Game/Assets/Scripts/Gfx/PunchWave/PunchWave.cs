c0c1fed0d9ff03d9dfc2e6e3ebdbafbb1cf0ef51
Mark Beiline <markbeiline@gmail.com>
2019-03-03 16:58:07 -0500

    A subtle blastwave for punching
    Usage is similar to stun stars effect: use the `PunchWave` prefab and call the `Play` method

--- Concordia-2-Game/Assets/Scripts/Gfx/PunchWave/PunchWave.cs
+++ Concordia-2-Game/Assets/Scripts/Gfx/PunchWave/PunchWave.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class PunchWave : MonoBehaviour
 {
-    MeshRenderer Renderer;
-    float Radius;
+    private MeshRenderer Renderer;
+    private float Radius;
+    private float StartTime;
+    private bool Playing = false;
 
     [Range(0, 1)]
-    public float Scale = 1.0f;
+    public float Playback = 0.0f;
+
+    public float Duration = 0.5f;
+
+    public AnimationCurve Scale;
+
+    public AnimationCurve Alpha;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +30,42 @@ public class PunchWave : MonoBehaviour
         Renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
     }
 
+    public void Play()
+    {
+        StartTime = Time.time;
+        Playing = true;
+    }
+
+    public bool IsPlaying()
+    {
+        return Playing;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Renderer.sharedMaterial.SetFloat("_RampTexInterp", Scale);
-        Renderer.sharedMaterial.SetFloat("_Radius", Radius * Scale);
+        if (!Playing)
+        {
+            Renderer.sharedMaterial.SetFloat("_Radius", 0.0f);
+            return;
+        }
+
+        // Playback
+        var curTime = Time.time;
+        var elapsed = curTime - StartTime;
+
+        if (elapsed > Duration) // Done animation
+            Playing = false;
+        
+        Playback = elapsed / Duration;
+        Playback = Mathf.Clamp01(Playback);
+
+
+        var scale = Scale.Evaluate(Playback);
+        var alpha = Alpha.Evaluate(Playback);
+
+        Renderer.sharedMaterial.SetFloat("_RampTexInterp", scale);
+        Renderer.sharedMaterial.SetFloat("_Radius", Radius * scale);
+        Renderer.sharedMaterial.SetFloat("_Alpha", alpha);
     }
 }
