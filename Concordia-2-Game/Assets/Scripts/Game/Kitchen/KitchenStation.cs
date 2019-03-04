ae2efcb928d8c2700c73bef01668f186eee21faa
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-04 15:13:15 -0500

    Added spinning spoon to cauldron prefab, used KitchenStation to toggle cooking fx, fixed missing references in Game scene for score, recipe, and discoball prefab

--- Concordia-2-Game/Assets/Scripts/Game/Kitchen/KitchenStation.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Kitchen/KitchenStation.cs
@@ -8,11 +8,17 @@ public class KitchenStation : MonoBehaviour
     private Ingredient m_storedIngredient;
     private RecipeManager m_recipeManager;
 
+    private Cauldron m_cauldronFX;
+    private Spin2Win m_spoonSpinner;
+
     private void Awake()
     {
         m_miniGame = GetComponent<ACookingMinigame>();
         m_recipeManager = GetComponent<RecipeManager>();
         m_storedIngredient = Ingredient.NOT_AN_INGREDIENT;
+
+        m_cauldronFX = GetComponent<Cauldron>();
+        m_spoonSpinner = GetComponentInChildren<Spin2Win>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,10 +28,14 @@ public class KitchenStation : MonoBehaviour
         {
             if (m_recipeManager == null || m_recipeManager.CollectIngredient(ingredient.m_ingredientType))    //If is generic station, or is cauldron and needs the ingredient 
             {
-                Debug.Log("Collected a " + ingredient.m_ingredientType);
                 m_storedIngredient = ingredient.m_ingredientType;
                 GameObject.Destroy(collision.gameObject);
                 m_miniGame.StartMinigame();
+                if (m_cauldronFX)
+                {
+                    m_cauldronFX.StartCooking();
+                    m_spoonSpinner.SetToSpin(true);
+                }
             }
         }
     }
@@ -43,6 +53,11 @@ public class KitchenStation : MonoBehaviour
         if (m_recipeManager)    //Is cauldron
         {
             m_recipeManager.ProcessIngredient(m_storedIngredient);  //Add to recipe
+            if (m_cauldronFX)
+            {
+                m_cauldronFX.StopCooking();
+                m_spoonSpinner.SetToSpin(false);
+            }
         }
         else                //Is some other kitchen station
         {
