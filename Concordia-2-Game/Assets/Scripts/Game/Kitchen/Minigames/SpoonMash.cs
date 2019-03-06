82a2d56a6ce79713d7f632bb9cccb7c1a11f53b4
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-06 18:44:06 -0500

    Missing untracked files again (as per the usual)

---
+++ Concordia-2-Game/Assets/Scripts/Game/Kitchen/Minigames/SpoonMash.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpoonMash : MonoBehaviour
+{
+    public GameObject m_mashPlacement;
+    Quaternion m_startOrientation, m_mashOrientation;
+    public float m_speed = 3;
+    private bool m_movingIntoPlace, m_mashing, m_resettingPos, m_gameIsOn;
+    private float m_desiredStartAngle = 225;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_mashOrientation = m_mashPlacement.transform.rotation;
+        m_startOrientation = transform.rotation;
+        EndMash();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_movingIntoPlace)
+        {
+            Debug.Log("moving into place");
+            if (Quaternion.Angle(transform.rotation, m_mashOrientation) < 5)
+            {
+                m_movingIntoPlace = false;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, m_mashOrientation, Time.deltaTime * m_speed);
+            }
+        }
+        else if (m_resettingPos)
+        {
+            Debug.Log("resetting");
+            if (Quaternion.Angle(transform.rotation, m_startOrientation) < 5)
+            {
+                m_resettingPos = false;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, m_startOrientation, Time.deltaTime * m_speed);
+            }
+        }
+        else if (m_gameIsOn)
+        {
+            Debug.Log("mashing");
+            transform.Rotate(Vector3.forward, m_speed);
+            if (m_mashing)
+            {
+                Vector3 pos = transform.position;
+                pos.y -= Time.deltaTime * m_speed;
+                transform.position = pos;
+
+                if (pos.y <= 0.2f)
+                    m_mashing = false;
+            }
+            else
+            {
+                Vector3 pos = transform.position;
+                pos.y += Time.deltaTime * m_speed;  
+
+                if (pos.y <= 0.4f)
+                    transform.position = pos;
+            }
+        }
+    }
+
+    public void StartMash()
+    {
+        m_gameIsOn = true;
+        m_movingIntoPlace = true;
+        m_resettingPos = false;
+    }
+
+    public void Mash()
+    {
+        m_mashing = true;
+    }
+
+    public void EndMash()
+    {
+        m_movingIntoPlace = false;
+        m_mashing = false;
+        m_resettingPos = true;
+        m_gameIsOn = false;
+    }
+
+}
+
