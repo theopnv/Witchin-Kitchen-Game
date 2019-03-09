3e8011ba2c2a984efc7df6b30447782c70aa665b
Mark Beiline <markbeiline@gmail.com>
2019-03-08 20:56:00 -0500

    Maintaining 8 different materials just to get color variation on cauldron and fireball is a drag
    This script can clone a material at scene startup and replace any amount of colors/textures on the cloned material
    
    It's not the most efficient but i'm sure we can afford it

---
+++ Concordia-2-Game/Assets/Scripts/Gfx/MaterialOverride/MaterialOverride.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOverride : MonoBehaviour
+{
+    [System.Serializable]
+    public enum MaterialOverrideEntryType
+    {
+        Color,
+        Texture,
+    }
+
+    [System.Serializable]
+    public class MaterialOverrideEntry
+    {
+        public MaterialOverrideEntryType Type;
+        public string Name;
+        public Color Color;
+        public Texture2D Texture;
+    }
+    
+    public List<MaterialOverrideEntry> Entries = new List<MaterialOverrideEntry>();
+
+    protected Renderer OurRenderer;
+    protected Material SourceMaterial;
+    protected Material ClonedMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloneMaterial();
+        Apply();
+    }
+
+    protected void CloneMaterial()
+    {
+        OurRenderer = gameObject.GetComponent<Renderer>();
+        SourceMaterial = OurRenderer.sharedMaterial;
+        ClonedMaterial = new Material(SourceMaterial);
+        OurRenderer.sharedMaterial = ClonedMaterial;
+    }
+
+    public void Apply()
+    {
+        foreach (var entry in Entries)
+        {
+            if (entry.Name == null)
+            {
+                throw new System.Exception("No property name set");
+            }
+
+            if (!ClonedMaterial.HasProperty(entry.Name))
+            {
+                throw new System.Exception("No material property with name: " + entry.Name);
+            }
+
+            if (entry.Type == MaterialOverrideEntryType.Color)
+            {
+                if (entry.Color == null)
+                {
+                    throw new System.Exception("No color set");
+                }
+                else
+                {
+                    ClonedMaterial.SetColor(entry.Name, entry.Color);
+                }
+            }
+            else if (entry.Type == MaterialOverrideEntryType.Texture)
+            {
+                if (entry.Texture == null)
+                {
+                    throw new System.Exception("No texture set");
+                }
+                else
+                {
+                    ClonedMaterial.SetTexture(entry.Name, entry.Texture);
+                }
+            }
+            else
+            {
+                throw new System.Exception("No type set on entry");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
