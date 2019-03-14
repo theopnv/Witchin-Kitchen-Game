33341b690e1c6183d09b2bd7112e651670ef1a31
VictorDebray <victor.debray@epitech.eu>
2019-03-14 15:50:48 -0400

    Random number suite for better repartition of ads

--- Concordia-2-Game/Assets/Scripts/Game/Audience/Events/EventImplementations/NetworkAdsManager.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Audience/Events/EventImplementations/NetworkAdsManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ProBuilder2.Common;
 using UnityEngine;
 
 namespace con2.game
@@ -16,21 +18,46 @@ namespace con2.game
         private Ad _Ketchup;
 
         [SerializeField]
-        private Ad _Pfc;
+        private Ad _FireBlades;
 
         [SerializeField]
-        private Ad _Ruffles;
+        private Ad _Apron;
+
+        private static int[] _suiteNumbers = UniqueRandomNumbers(0, 3);
+        private static int _index = 0;
+        private static int _numberOfAds = 4; 
+
+        public static int[] UniqueRandomNumbers(int min, int max)
+        {
+            var rnd = new System.Random();
+            int[] numbers = new int[max - min + 1];
+            var toBeInserted = new List<int>();
+            for (int i = min; i <= max; i++)
+            {
+                toBeInserted.Add(i);
+            }
+
+            var idx = 0;
+            while (toBeInserted.Count != 0)
+            {
+                var num = rnd.Next(min, max - idx);
+                numbers[idx] = toBeInserted.ElementAt(num);
+                toBeInserted.RemoveAt(num);
+                idx++;
+            }
+
+            return numbers;
+        }
 
         // Start is called before the first frame update
         void Start()
         {
             _BreakingNews.gameObject.SetActive(false);
             _Ketchup.gameObject.SetActive(false);
-            _Pfc.gameObject.SetActive(false);
-            _Ruffles.gameObject.SetActive(false);
-            var rnd = new System.Random();
-            var num = rnd.Next(0, 2);
-            switch (num)
+            _FireBlades.gameObject.SetActive(false);
+            _Apron.gameObject.SetActive(false);
+
+            switch (_suiteNumbers[_index])
             {
                 case 0:
                     _BreakingNews.gameObject.SetActive(true);
@@ -39,13 +66,17 @@ namespace con2.game
                     _Ketchup.gameObject.SetActive(true);
                     break;
                 case 2:
-                    _Pfc.gameObject.SetActive(true);
+                    _FireBlades.gameObject.SetActive(true);
                     break;
                 case 3:
-                    _Ruffles.gameObject.SetActive(true);
+                    _Apron.gameObject.SetActive(true);
                     break;
 
             }
+
+            ++_index;
+            if (_index == _numberOfAds)
+                _index = 0;
         }
 
         // Update is called once per frame
