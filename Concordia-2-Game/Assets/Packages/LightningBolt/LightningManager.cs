2fdd48f4a5cb038f71dfe5bd451048dedd6da5ea
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-16 11:46:01 -0400

    WIPPERONI

---
+++ Concordia-2-Game/Assets/Packages/LightningBolt/LightningManager.cs
@@ -0,0 +1,23 @@
+﻿using DigitalRuby.LightningBolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace con2.game
+{
+    public class LightningManager : MonoBehaviour
+    {
+        private LightningBoltScript m_lightningThing;
+
+        void Start()
+        {
+            m_lightningThing = GetComponent<LightningBoltScript>();
+        }
+
+        void OnSpellCast(PlayerManager targetPlayer)
+        {
+            m_lightningThing.EndObject = targetPlayer.gameObject;
+            m_lightningThing.Trigger();
+        }
+    }
+}
