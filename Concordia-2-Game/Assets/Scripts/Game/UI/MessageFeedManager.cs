4d7f7a3e81fc2c2d68fc7e0de6767b880b1b549d
Theo Penavaire <theo.penavaire@gmail.com>
2019-03-18 15:44:51 -0400

    Refine message feed

--- Concordia-2-Game/Assets/Scripts/Game/UI/MessageFeedManager.cs
+++ Concordia-2-Game/Assets/Scripts/Game/UI/MessageFeedManager.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace con2.game
 {
-
     public class MessageFeedManager : MonoBehaviour
     {
         public enum MessageType
@@ -19,19 +19,11 @@ namespace con2.game
         [SerializeField]
         private GameObject _MessageFeedPlaceholder;
 
-        private List<Text> _MessageTexts;
+        private Queue<GameObject> _Messages;
 
         [SerializeField]
         private GameObject _GlobalMessagePrefab;
 
-        private float _LastMessageIntantiationTime;
-
-        private Dictionary<MessageType, Color> MessageTypeLookupTable = new Dictionary<MessageType, Color>
-        {
-            { MessageType.error, Color.red },
-            { MessageType.arena_event, Color.cyan },
-            { MessageType.generic, Color.white },
-        };
 
         // Public
         public float TimeBeforeFade = 15f;
@@ -41,59 +33,17 @@ namespace con2.game
         // Start is called before the first frame update
         void Start()
         {
-            _MessageTexts = new List<Text>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            _LastMessageIntantiationTime += Time.deltaTime;
-            if (_MessageTexts.Count > 0 && _LastMessageIntantiationTime >= TimeBeforeFade)
-            {
-                _LastMessageIntantiationTime = 0f; // Even if no text spawned, this prevents re-entering the condition at the next frame and therefore accelerating the fading effect.
-                StartCoroutine(FadeOut());
-            }
+            _Messages = new Queue<GameObject>();
         }
 
         #endregion
 
         #region Custom methods
 
-        private IEnumerator FadeOut()
+        private void OnDestroyMessage(GlobalMessage message)
         {
-            var fadeOutDuration = 5f;
-            for (var t = 0.01f; t < fadeOutDuration; t += Time.deltaTime)
-            {
-                foreach (var msg in _MessageTexts)
-                {
-                    var originalColor = msg.color;
-                    msg.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(.25f, t / fadeOutDuration));
-                }
-
-                yield return null;
-            }
-            DestroyAllMessages();
-        }
-
-        private void DestroyFirstMessage()
-        {
-            if (_MessageTexts.Count == 0)
-            {
-                return;
-            }
-
-            var msg = _MessageTexts[0];
-            Destroy(msg.gameObject);
-            _MessageTexts.Remove(msg);
-        }
-
-        private void DestroyAllMessages()
-        {
-            foreach (var text in _MessageTexts)
-            {
-                Destroy(text);
-            }
-            _MessageTexts.Clear();
+            message.OnDestroy -= OnDestroyMessage;
+            Destroy(_Messages.Dequeue());
         }
 
         /// <summary>
@@ -103,19 +53,18 @@ namespace con2.game
         /// <param name="msgType"></param>
         public void AddMessageToFeed(string message, MessageType msgType)
         {
-            _LastMessageIntantiationTime = 0f;
             const int maxMessagesInFeed = 3;
 
-            if (_MessageTexts.Count >= maxMessagesInFeed)
+            if (_Messages.Count >= maxMessagesInFeed)
             {
-                DestroyFirstMessage();
+                Destroy(_Messages.Dequeue());
             }
 
-            var instance = Instantiate(_GlobalMessagePrefab, _MessageFeedPlaceholder.transform);
-            var text = instance.GetComponent<Text>();
-            text.color = MessageTypeLookupTable[msgType];
-            text.text = message;
-            _MessageTexts.Add(text);
+            var msg = Instantiate(_GlobalMessagePrefab.transform, _MessageFeedPlaceholder.transform).GetComponent<GlobalMessage>();
+            msg.Message = message;
+            msg.MsgType = msgType;
+            msg.OnDestroy += OnDestroyMessage;
+            _Messages.Enqueue(msg.gameObject);
         }
 
         #endregion
