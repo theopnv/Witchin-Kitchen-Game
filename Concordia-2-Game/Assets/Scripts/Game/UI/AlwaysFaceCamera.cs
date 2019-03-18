2d1671b34d1646207a132aa7b52da6e581c9c3d0
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-18 19:36:20 -0400

    Minigame UI icons always face the camera (to avoid the weird angle)

--- Concordia-2-Game/Assets/Scripts/Game/UI/AlwaysFaceCamera.cs
+++ Concordia-2-Game/Assets/Scripts/Game/UI/AlwaysFaceCamera.cs
@@ -4,10 +4,17 @@ using UnityEngine.UI;
 
 public class AlwaysFaceCamera : MonoBehaviour
 {
+    private Transform m_towardsCamera;
+
+    private void Start()
+    {
+        m_towardsCamera = Camera.main.transform;
+    }
+
     void FixedUpdate()
     {
-        transform.LookAt(Camera.main.transform);
-        var prompt = transform.Find("Backdrop");
+        transform.LookAt(m_towardsCamera);
+        var prompt = transform.Find("Canvas/Backdrop");
         prompt.transform.localEulerAngles = new Vector3(0, 180, 0);
     }
 }
