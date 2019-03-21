a05376c62730a3e459e21c876946932683de0628
Mark Beiline <markbeiline@gmail.com>
2019-03-20 23:35:46 -0400

    Made a better outline effect (following https://www.artstation.com/artwork/qy8Ge) Much much cleaner and works well with TAA (no more sucky stuff around the edges) The pumpkin has a thin outline for now, gotta get rid of the interior faces or else they cover the main model :O

---
+++ Concordia-2-Game/Assets/Scripts/Gfx/Outline2/Outline2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline2 : MonoBehaviour
+{
+    public Material OutlineMaterial;
+
+    [Range(0.0f, 1.0f)]
+    public float Thickness = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var children = GetComponentsInChildren<Outline2Marker>();
+        foreach (var child in children)
+        {
+            var childRenderer = child.gameObject.GetComponent<Renderer>();
+
+            var clone = Instantiate(childRenderer.gameObject);
+            clone.transform.SetParent(gameObject.transform, false);
+
+            var cloneRenderer = clone.GetComponent<Renderer>();
+            cloneRenderer.sharedMaterial = OutlineMaterial;
+            cloneRenderer.receiveShadows = false;
+            cloneRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+            var curScale = clone.transform.localScale;
+            var extents = cloneRenderer.bounds.extents;
+
+            var newScaleX = curScale.x * (extents.x + Thickness) / extents.x;
+            var newScaleY = curScale.y * (extents.y + Thickness) / extents.y;
+            var newScaleZ = curScale.z * (extents.z + Thickness) / extents.z;
+
+            clone.transform.localScale = new Vector3(newScaleX, newScaleY, newScaleZ);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Concordia-2-Game/Assets/Scripts/Gfx/Outline2/Outline2.shader
@@ -0,0 +1,38 @@
+﻿Shader "Unlit/Outline2"
+{
+    Properties
+    {
+        _OutlineColor("Outline color", Color) = (1, 1, 1, 1)
+        _OutlineEmissiveFactor("Outline emissive factor", Float) = 1.0
+    }
+
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" }
+        Cull Front
+        CGPROGRAM
+        #pragma surface surf Lambert vertex:vert
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        float4 _OutlineColor;
+        float _OutlineEmissiveFactor;
+
+        void vert (inout appdata_full v)
+        {
+            v.normal.xyz = v.normal * -1;
+        }
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+            o.Albedo = float3(0.0, 0.0, 0.0);
+            o.Emission = _OutlineColor.rgb * _OutlineEmissiveFactor;
+            o.Alpha = 1.0;
+        }
+        ENDCG
+    } 
+    Fallback "Diffuse"
+}
