590473f9e2da59ed822afd8e8e2e4e7a48e62b56
Theo Penavaire <theo.penavaire@gmail.com>
2019-03-21 10:21:58 -0400

    Fix a bunch of NullReferenceExceptions

--- Concordia-2-Game/Assets/Scripts/Game/SharedCamera.cs
+++ Concordia-2-Game/Assets/Scripts/Game/SharedCamera.cs
@@ -52,7 +52,7 @@ namespace con2.game
             }
 
             //Average the positions with the x3 to compensate for the middle
-            middle += m_zoomFactor*m_initialDistanceFromCenter.normalized*furthestFromMiddle.magnitude;  //zoom
+            middle += m_zoomFactor * m_initialDistanceFromCenter.normalized * furthestFromMiddle.magnitude;  //zoom
             middle += m_initialDistanceFromCenter;  //Keeps camera above the arena, pointed at 'middle'
             return middle;
         }
