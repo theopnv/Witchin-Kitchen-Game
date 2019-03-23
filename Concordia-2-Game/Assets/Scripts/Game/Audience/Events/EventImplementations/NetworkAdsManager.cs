7b4a5834b727cf142beaa08d5dba7f109fd2c8e6
VictorDebray <victor.debray@epitech.eu>
2019-03-23 18:03:20 -0400

    Ads are spread across the screen

--- Concordia-2-Game/Assets/Scripts/Game/Audience/Events/EventImplementations/NetworkAdsManager.cs
+++ Concordia-2-Game/Assets/Scripts/Game/Audience/Events/EventImplementations/NetworkAdsManager.cs
@@ -17,10 +17,22 @@ namespace con2.game
         private Ad _combo1Apron;
 
         [SerializeField]
+        private Ad _combo1FireBladesUp;
+
+        [SerializeField]
+        private Ad _combo1ApronDown;
+
+        [SerializeField]
         private Ad _combo2FireBlades;
 
         [SerializeField]
         private Ad _combo2Apron;
+
+        [SerializeField]
+        private Ad _combo2FireBladesDown;
+
+        [SerializeField]
+        private Ad _combo2ApronUp;
 
         [SerializeField]
         private Ad _combo3FireBlades;
@@ -29,10 +41,22 @@ namespace con2.game
         private Ad _combo3Apron;
 
         [SerializeField]
+        private Ad _combo3FireBladesUp;
+
+        [SerializeField]
+        private Ad _combo3ApronDown;
+
+        [SerializeField]
         private Ad _combo4FireBlades;
 
         [SerializeField]
         private Ad _combo4Apron;
+
+        [SerializeField]
+        private Ad _combo4FireBladesDown;
+
+        [SerializeField]
+        private Ad _combo4ApronUp;
 
         private static readonly int[] SuiteNumbers = UniqueRandomNumbers(0, 3);
         private static int _index = 0;
@@ -65,30 +89,50 @@ namespace con2.game
         {
             _combo1FireBlades.gameObject.SetActive(false);
             _combo1Apron.gameObject.SetActive(false);
+            _combo1FireBladesUp.gameObject.SetActive(false);
+            _combo1ApronDown.gameObject.SetActive(false);
+
             _combo2FireBlades.gameObject.SetActive(false);
             _combo2Apron.gameObject.SetActive(false);
+            _combo2FireBladesDown.gameObject.SetActive(false);
+            _combo2ApronUp.gameObject.SetActive(false);
+
             _combo3FireBlades.gameObject.SetActive(false);
             _combo3Apron.gameObject.SetActive(false);
+            _combo3FireBladesUp.gameObject.SetActive(false);
+            _combo3ApronDown.gameObject.SetActive(false);
+
             _combo4FireBlades.gameObject.SetActive(false);
             _combo4Apron.gameObject.SetActive(false);
+            _combo4FireBladesDown.gameObject.SetActive(false);
+            _combo4ApronUp.gameObject.SetActive(false);
 
-            switch (SuiteNumbers[_index])
+            //switch (SuiteNumbers[_index])
+            switch (3)
             {
                 case 0:
                     _combo1FireBlades.gameObject.SetActive(true);
                     _combo1Apron.gameObject.SetActive(true);
+                    _combo1FireBladesUp.gameObject.SetActive(true);
+                    _combo1ApronDown.gameObject.SetActive(true);
                     break;
                 case 1:
                     _combo2FireBlades.gameObject.SetActive(true);
                     _combo2Apron.gameObject.SetActive(true);
+                    _combo2FireBladesDown.gameObject.SetActive(true);
+                    _combo2ApronUp.gameObject.SetActive(true);
                     break;
                 case 2:
                     _combo3FireBlades.gameObject.SetActive(true);
                     _combo3Apron.gameObject.SetActive(true);
+                    _combo3FireBladesUp.gameObject.SetActive(true);
+                    _combo3ApronDown.gameObject.SetActive(true);
                     break;
                 case 3:
                     _combo4FireBlades.gameObject.SetActive(true);
                     _combo4Apron.gameObject.SetActive(true);
+                    _combo4FireBladesDown.gameObject.SetActive(true);
+                    _combo4ApronUp.gameObject.SetActive(true);
                     break;
 
             }
