bab62815032e9e3799dfb79edfbc14fc50b08428
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-24 10:53:49 -0400

    Menu versions of char models with simple animations

---
+++ Concordia-2-Game/Assets/Models/Characters/Anims/MenuModelAnims.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuModelAnims : MonoBehaviour
+{
+    protected Animator Anim;
+
+    protected int CarryTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+        CarryTrigger = Animator.StringToHash("Carry");
+    }
+
+    public void Carry()
+    {
+        SetTrigger(CarryTrigger);
+    }
+
+    protected void SetTrigger(int trigger)
+    {
+        Anim.SetTrigger(trigger);
+    }
+
+}
