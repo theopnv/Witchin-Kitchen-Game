9e84c685844ed23340e06c425ed6ea02a270a9cf
Nicole Parmentier <nclparmentier@gmail.com>
2019-03-26 19:06:03 -0400

    4got dis

---
+++ Concordia-2-Game/Assets/Scripts/Game/UI/Flash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Flash : MonoBehaviour
+{
+    private Image thingToFlash;
+
+    void Start()
+    {
+        thingToFlash = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        if (SceneManager.GetActiveScene().name == con2.SceneNames.Lobby)
+        {
+            StartCoroutine(FlashParty());
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator FlashParty()
+    {
+        bool tempEnabled = false;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.75f);
+            thingToFlash.enabled = tempEnabled;
+            tempEnabled = !tempEnabled;
+        }
+    }
+}
