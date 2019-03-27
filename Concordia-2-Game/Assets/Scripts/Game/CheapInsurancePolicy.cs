39da9d417186f522ead07bda43064b34eabde9a7
Mark Beiline <markbeiline@gmail.com>
2019-03-27 17:29:15 -0400

    When huge forces are involved then things can manage to fly through our bound colliders.
    One way to solve it is to set continuous collision detection on our RigidBodies.
    
    Another way is to add this script to our ingredient and player prefabs (script should be on the same object as the RigidBody component). I haven't added it to the prefabs to avoid merge conflicts, it can be done later. I did test it though, works great.

---
+++ Concordia-2-Game/Assets/Scripts/Game/CheapInsurancePolicy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheapInsurancePolicy : MonoBehaviour
+{
+    // DON'T FORGET TO CHANGE IF BOUNDS CHANGE FOR SOME REASON!
+    // In world position (X=left/right, Y=bottom/top, Z=front/back):
+    static public Vector3 Min = new Vector3(-22, -40, -16);
+    static public Vector3 Max = new Vector3(22, 40, 16);
+
+    // The teleport point. Should be above the camera's view but below the top bounding wall.
+    static public Vector3 RespawnPoint = new Vector3(0.0f, 20.0f, 0.0f);
+
+    protected Rigidbody Body;
+    
+    void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+
+        if (pos.x > Min.x && pos.y > Min.y && pos.z > Min.z && pos.x < Max.x && pos.y < Max.y && pos.z < Max.z)
+        {
+            // All good
+        }
+        else
+        {
+            // Insurance coverage kicks in
+
+            transform.position = RespawnPoint;      // Teleport inside the arena
+            Body.velocity = Vector3.zero;           // Cancel whatever caused this in the first place
+        }
+    }
+}
