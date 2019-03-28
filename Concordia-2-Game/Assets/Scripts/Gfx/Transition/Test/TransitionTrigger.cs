70472041e4c1bcd2ef4b71b8d5ec9bdb598d408b
Mark Beiline <markbeiline@gmail.com>
2019-03-27 20:34:13 -0400

    Made fade thingy

---
+++ Concordia-2-Game/Assets/Scripts/Gfx/Transition/Test/TransitionTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace con2
+{
+    public class TransitionTrigger : MonoBehaviour
+    {
+        public Transition Target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                StartCoroutine(Target.PlayIn());
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                StartCoroutine(Target.PlayOut());
+            }
+        }
+    }
+}
