9ef31249da889f94ffde02de6c3bab28c69dbd31
Mark Beiline <markbeiline@gmail.com>
2019-03-30 14:24:06 -0400

    Added a gravity script to emulate mass

---
+++ Concordia-2-Game/Assets/Prefabs/Game/Characters/HairGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairGravity : MonoBehaviour
+{
+    protected Rigidbody Body;
+    public float Gravity = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Body.AddForce(Vector3.down * Gravity, ForceMode.Acceleration);
+    }
+}
